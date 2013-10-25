55814f9feefbe1e0c4aa99dbbc5ca8ad6fb39862
Randy Zwitch <rzwitch@gmail.com>
2013-10-24 22:25:42 -0400

    Parse YAML into 3 arrays
    Parse YAML into device, os, and user-agent arrays for easier use

--- src/UAParser.jl
+++ src/UAParser.jl
@@ -7,6 +7,38 @@ module UAParser
 ##############################################################################
 
 using YAML
-using DataFrames
+export device_regex, user_agent_regex, os_regex
+
+
+##############################################################################
+##
+## Load regexes.yaml, which is the basis for entire package
+##
+##############################################################################
+
+
+function init()
+	global device_regex
+	global user_agent_regex
+	global os_regex
+
+	#Load the YAML file, creating a global object "regexes"
+	regexes = YAML.load(open(Pkg.dir("UAParser", "regexes.yaml")));
+
+	#Break into device, user agent, and os arrays
+	device_regex = regexes["device_parsers"];
+	user_agent_regex = regexes["user_agent_parsers"];
+	os_regex = regexes["os_parsers"];
+
+	return device_regex, user_agent_regex, os_regex
+end
+
+
+
+
+
+
+
+
 
 end # module
