79cb188e1f0b9c08d36318e499248171a4675cfe
Randy Zwitch <rzwitch@gmail.com>
2018-12-19 11:24:17 -0500

    Update test files

--- test/runtests.jl
+++ test/runtests.jl
@@ -28,7 +28,7 @@ end
 
 @testset "parse_os" begin
     #Test 2: Validation of parseos
-    test_os = YAML.load(open(joinpath(dirname(@__FILE__), "data", "test_user_agent_parser_os.yaml")));
+    test_os = YAML.load(open(joinpath(dirname(@__FILE__), "data", "test_os.yaml")));
 
     for value in test_os["test_cases"]
         os = parseos(value["user_agent_string"])
@@ -39,37 +39,11 @@ end
     end
 end
 
-@testset "parse_os2" begin
-    #Test 3: Additional validation of parseos
-    test_os_2 = YAML.load(open(joinpath(dirname(@__FILE__), "data", "additional_os_tests.yaml")));
-
-    for value in test_os_2["test_cases"]
-        os = parseos(value["user_agent_string"])
-        @test value["family"] == os.family
-        @testparseval value "major" os
-        @testparseval value "minor" os
-        @testparseval value "patch_minor" os
-    end
-end
-
 @testset "parse_ua" begin
     #Test 4: Validation of parseuseragent
-    test_ua = YAML.load(open(joinpath(dirname(@__FILE__), "data", "test_user_agent_parser.yaml")));
+    test_ua = YAML.load(open(joinpath(dirname(@__FILE__), "data", "test_ua.yaml")));
 
     for value in test_ua["test_cases"]
-        ua = parseuseragent(value["user_agent_string"])
-        @test value["family"] == ua.family
-        @testparseval value "major" ua
-        @testparseval value "minor" ua
-        @testparseval value "patch" ua
-    end
-end
-
-@testset "parse_ua2" begin
-    #Test 5: Additional validation of parseuseragent
-    test_ua_2 = YAML.load(open(joinpath(dirname(@__FILE__), "data", "firefox_user_agent_strings.yaml")));
-
-    for value in test_ua_2["test_cases"]
         ua = parseuseragent(value["user_agent_string"])
         @test value["family"] == ua.family
         @testparseval value "major" ua
